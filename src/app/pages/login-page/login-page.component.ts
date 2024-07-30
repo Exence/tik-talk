ae81d9e19add961915bb805a494a750dacad5c99
Алишер Мансуров <alisermansurov@MacBook-Pro-Alisher.local>
2024-07-30 21:11:18 +0300

    style: add show passwd btn

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
 import { Router } from '@angular/router';
@@ -18,6 +18,8 @@ export class LoginPageComponent {
 
   authService = inject(AuthService)
   router = inject(Router)
+
+  isPasswordVisible = signal<boolean>(false)
 
   onSubmit() {
     if (this.loginForm.valid) {
