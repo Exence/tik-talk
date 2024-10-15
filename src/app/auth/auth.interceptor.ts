cc6b9e2dd93931d42734a55e919dd09c223330df
Алишер Мансуров <alisermansurov@MacBook-Pro-Alisher.local>
2024-10-15 21:21:15 +0300

    refactor: add small changes into auth service

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,25 +1,36 @@
 import { HttpInterceptorFn, HttpRequest } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { AuthService } from "./auth.service";
-import { catchError, switchMap, throwError } from "rxjs";
+import { BehaviorSubject, catchError, filter, switchMap, tap, throwError } from "rxjs";
 
-let isRefreshing = false
+let isRefreshing$ = new BehaviorSubject<boolean>(false)
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService)
   const accessToken = authService.accessToken
  
-  if (!accessToken || isRefreshing) {
-    isRefreshing = false
+  if (!accessToken || req.url.includes('refresh')) {
     return next(req)
   } 
+
+  if (isRefreshing$.value) {
+    isRefreshing$.pipe(
+      filter(isRefreshing => !isRefreshing),
+      switchMap(res =>{
+        return next(addAuthHeaders(req, accessToken))
+      })
+    )
+  }
 
   return next(addAuthHeaders(req, accessToken)).pipe(
     catchError(err => {
       if (err.status === 403) {
+        isRefreshing$.next(true)
         return authService.getRefreshedToken().pipe(
           switchMap((res) => {
-            return next(addAuthHeaders(req, res.access_token))
+            return next(addAuthHeaders(req, res.access_token)).pipe(
+              tap(() => {isRefreshing$.next(false)})
+            )
           })
         )
       }
