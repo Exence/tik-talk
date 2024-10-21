cd416c466ce283044e73b35a069b228252c24656
Алишер Мансуров <alisermansurov@MacBook-Pro-Alisher.local>
2024-10-21 19:22:59 +0300

    chore: add time ago pipe

--- src/app/pages/profile-page/post-feed/single-comment/single-comment.component.ts
+++ src/app/pages/profile-page/post-feed/single-comment/single-comment.component.ts
@@ -1,19 +1,15 @@
 import { Component, input } from '@angular/core';
 import { CircleAvatarComponent } from "../../../../common-ui/circle-avatar/circle-avatar.component";
 import { Comment } from '../../../../data/interfaces/post.interface';
-import { getCreateOrUpdatePostDateString } from '../../../../helpers/time-ago';
+import { TimeAgoPipe } from "../../../../helpers/pipes/time-ago.pipe";
 
 @Component({
   selector: 'app-single-comment',
   standalone: true,
-  imports: [CircleAvatarComponent],
+  imports: [CircleAvatarComponent, TimeAgoPipe],
   templateUrl: './single-comment.component.html',
   styleUrl: './single-comment.component.scss'
 })
 export class SingleCommentComponent {
   comment = input<Comment>()
-
-  getCommentTimeString(createdAt: string, updatedAt: string | null | undefined) {
-    return getCreateOrUpdatePostDateString(createdAt, updatedAt)
-  }
 }
--- src/app/pages/profile-page/post-feed/single-post/single-post.component.ts
+++ src/app/pages/profile-page/post-feed/single-post/single-post.component.ts
@@ -1,17 +1,17 @@
 import { Component, ElementRef, inject, input, Input, OnInit, Renderer2, signal } from '@angular/core';
 import { Post, CommentCreateDto, Comment } from '../../../../data/interfaces/post.interface';
 import { CircleAvatarComponent } from "../../../../common-ui/circle-avatar/circle-avatar.component";
-import { getCreateOrUpdatePostDateString } from '../../../../helpers/time-ago';
 import { SvgIconComponent } from '../../../../common-ui/svg-icon/svg-icon.component';
 import { PostInputComponent } from "../../../../common-ui/post-input/post-input.component";
 import { PostService } from '../../../../data/services/post.service';
 import { firstValueFrom, tap } from 'rxjs';
 import { SingleCommentComponent } from "../single-comment/single-comment.component";
+import { TimeAgoPipe } from "../../../../helpers/pipes/time-ago.pipe";
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [CircleAvatarComponent, SvgIconComponent, PostInputComponent, SingleCommentComponent],
+  imports: [CircleAvatarComponent, SvgIconComponent, PostInputComponent, SingleCommentComponent, TimeAgoPipe],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.scss'
 })
@@ -36,9 +36,5 @@ export class SinglePostComponent implements OnInit{
     firstValueFrom(this.postService.getCommenstByPostId(comment.postId)).then(
       res => this.comments.set(res)
     )
-  }
-
-  getPostTimeString(createdAt: string, updatedAt: string | null | undefined) {
-    return getCreateOrUpdatePostDateString(createdAt, updatedAt)
   }
 }
